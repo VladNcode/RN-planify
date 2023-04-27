16d963950d66c37f30cc4482a8bc78243e3d02fd
vladncode <vladncode@gmail.com>
2023-04-27 21:16:11 +0300

    Added PP & TC links and input types

---
+++ src/constants/links.ts
@@ -0,0 +1,2 @@
+export const TERMS_AND_CONDITIONS_LINK = 'https://www.freeprivacypolicy.com/live/9d825197-e3cd-4a07-abbf-fb0113c76bc7';
+export const PRIVACY_POLICY_LINK = 'https://www.freeprivacypolicy.com/live/5884976f-4d5c-4fda-a35d-3c18522f5b5d';
--- src/screens/auth/Signup/index.tsx
+++ src/screens/auth/Signup/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { Linking, SafeAreaView, Text, View, TouchableOpacity } from 'react-native';
 
 import { CustomButton } from '../../../components/Button';
+import { Checkbox } from '../../../components/Checkbox';
 import { FooterLink } from '../../../components/FooterLink';
 import { Input } from '../../../components/Input';
 import { Title } from '../../../components/Title';
+import { PRIVACY_POLICY_LINK, TERMS_AND_CONDITIONS_LINK } from '../../../constants/links';
 import { SignupNavigationProp } from '../../../constants/navigation.types';
 import { styles } from './styles';
 
@@ -17,6 +19,10 @@ export const Signup = React.memo(({ navigation }: { navigation: SignupNavigation
     navigation.navigate('Signin');
   };
 
+  const onLinkPress = (url: string) => {
+    Linking.openURL(url);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Title text="Join the hub!" />
@@ -24,9 +30,24 @@ export const Signup = React.memo(({ navigation }: { navigation: SignupNavigation
       <View style={styles.inputsContainer}>
         <Input placeholder="First Name" />
         <Input style={styles.input} placeholder="Last Name" />
-        <Input style={styles.input} placeholder="Email" />
-        <Input style={styles.input} placeholder="Password" />
-        <Input style={styles.input} placeholder="Confirm Password" />
+        <Input style={styles.input} keyboardType="email-address" placeholder="Email" />
+        <Input style={styles.input} secureTextEntry placeholder="Password" />
+        <Input style={styles.input} secureTextEntry placeholder="Confirm Password" />
+      </View>
+
+      <View style={styles.checkboxContainer}>
+        <Checkbox />
+
+        <View style={styles.checkboxTextContainer}>
+          <Text style={styles.checkboxText}>I agree to the </Text>
+          <TouchableOpacity onPress={() => onLinkPress(TERMS_AND_CONDITIONS_LINK)}>
+            <Text style={styles.link}>Terms of Service </Text>
+          </TouchableOpacity>
+          <Text style={styles.checkboxText}>and </Text>
+          <TouchableOpacity onPress={() => onLinkPress(PRIVACY_POLICY_LINK)}>
+            <Text style={styles.link}>Privacy Policy</Text>
+          </TouchableOpacity>
+        </View>
       </View>
 
       <CustomButton style={styles.button} onPress={onPress}>
