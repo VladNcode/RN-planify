d8b3f0ff8bd8de262eb7f3abda689ae454a4df4f
vladncode <vladncode@gmail.com>
2023-05-08 21:32:08 +0300

    Added tasks slice

--- src/screens/app/Home/index.tsx
+++ src/screens/app/Home/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import firestore from '@react-native-firebase/firestore';
+import React, { useEffect } from 'react';
 import { SafeAreaView, ScrollView } from 'react-native';
 
 import { Header } from '../../../components/Header';
 import { PlusIcon } from '../../../components/PlusIcon';
 import { Title } from '../../../components/Title';
 import { HomeNavigationProp, HomeRoute } from '../../../constants/navigation.types';
+import { useAppDispatch, useAppSelector } from '../../../hooks/reduxHooks';
+import { selectUser } from '../../../store/userSlice';
 import { styles } from './styles';
+import { Task, selectTasks, setFirebaseTasks } from '../../../store/tasksSlice';
 
 interface HomeProps {
   navigation: HomeNavigationProp;
@@ -13,6 +17,29 @@ interface HomeProps {
 }
 
 export const Home = React.memo(({ navigation, route }: HomeProps) => {
+  const dispatch = useAppDispatch();
+  const user = useAppSelector(selectUser);
+
+  const userRef = firestore().collection('Users').doc(user?.uid);
+  const tasksQuery = userRef.collection('Tasks');
+
+  useEffect(() => {
+    const subscribe = tasksQuery.onSnapshot(docSnapshot => {
+      const tasksArray: Task[] = [];
+
+      docSnapshot.forEach(doc => {
+        tasksArray.push(doc.data() as Task);
+      });
+
+      dispatch(setFirebaseTasks(tasksArray));
+      console.log('Tasks updated :>>>>\n', tasksArray);
+    });
+
+    return () => {
+      subscribe();
+    };
+  }, [user]);
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
